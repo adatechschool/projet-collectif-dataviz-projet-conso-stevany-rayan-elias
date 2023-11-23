2f1091eb432a454af3effb94f770c90bc7ac0724
Rayan Fraboulet <ryn@MacBook-Air-de-Rayan.local>
2023-11-23 16:30:57 +0100

    dernière verssion

--- charity.js
+++ charity.js
@@ -1,27 +1,32 @@
-let name = document.getElementById("name");
-let image = document.getElementById("image");
-let button = document.getElementById("button");
-let lieu = document.getElementById("lieux");
+const button = document.getElementById("submit");
+const Balance = document.getElementById("SigneAstro")
 
-const requestString = `https://journal-officiel-datadila.opendatasoft.com/api/explore/v2.1/catalog/datasets/jo_associations/records?limit=20&refine=localisation_facette%3A%22%C3%8Ele-de-France%2FParis%22&refine=domaine_activite_libelle_categorise%3A%22culture%2C%20pratiques%20d%E2%80%99activit%C3%A9s%20artistiques%2C%20culturelles%2Fphotographie%2C%20cin%C3%A9ma%20(dont%20cin%C3%A9-clubs)%22&refine=domaine_activite_libelle_categorise%3A%22culture%2C%20pratiques%20d%E2%80%99activit%C3%A9s%20artistiques%2C%20culturelles%22&refine=domaine_activite_libelle_categorise%3A%22Sports%2C%20activit%C3%A9s%20de%20plein%20air%2FMarche%20sportive%20(randonn%C3%A9e%20p%C3%A9destre%2C%20raid%2C%20trekking%2C%20course%20orientation)%22`
- 
- const charity = async () => {
-    let data = await fetch (requestString)
-    let response = await data.json()
+
+console.log(button)
+
+
+
+
+
+
+//jour     https://kayoo123.github.io/astroo-api/jour.json
+
+//semaine   https://kayoo123.github.io/astroo-api/hebdomadaire.jso:n
+
+let dateJour = document.getElementById("dateJour");
+let dateSemaine = document.getElementById("dateSemaine");
+
+
+const euroscopeJour = async () => {
+    const requestsString = `https://kayoo123.github.io/astroo-api/jour.json`
+    let data = await fetch (requestsString);
+    let response = await data.json();
     console.log(response)
- }
-
- const requestString2 = "/api/explore/v2.1/catalog/datasets/jo_associations/records?limit=20&refine=localisation_facette%3A%22%C3%8Ele-de-France%2FParis%22&refine=domaine_activite_libelle_categorise%3A%22culture%2C%20pratiques%20d%E2%80%99activit%C3%A"
-
-const newCharity = async () => {
-    let data = await fetch (requestString2)
-    let response = await data.json()
-    console.log(response)
+    Balance.textContent = response.balance
 }
 
- domaine_activite_libelle_categorise :[
-    "Sports, activités de plein air/Marche sportive (randonnée pédestre, raid, trekking, course orientation)",
-    "culture, pratiques d’activités artistiques, culturelles/photographie, cinéma (dont ciné-clubs)"
-    ],
 
-charity();
+
+
+euroscopeJour();
+button.addEventListener("click", euroscopeJour());
